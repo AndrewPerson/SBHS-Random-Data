ab754e67b6f9fadf6c22283a74b7cfabb605f9e8
Andrew Person <professor_smart@hotmail.com>
2021-10-18 10:10:09 +1100

    Added grouped endpoint

--- server.js
+++ server.js
@@ -13,4 +13,29 @@ server.get("/api/timetable/daytimetable.json", dailytimetable);
 server.get("/api/timetable/timetable.json", timetable);
 server.get("/api/details/userinfo.json", userinfo);
 
+server.get("/all.json", async () => {
+    var expiry = new Date();
+    expiry.setHours(expiry.getHours() + 1);
+
+    var termination = new Date();
+    //90 Days
+    termination.setHours(termination.getHours() + 90 * 24);
+
+    return {
+        result: {
+            announcements: await announcements(),
+            dailytimetable: await dailytimetable(),
+            "next-dailytimetable": await dailytimetable(),
+            timetable: await timetable(),
+            userinfo: userinfo()
+        },
+        token: {
+            access_token: "",
+            refresh_token: "",
+            expiry: expiry.toISOString(),
+            termination: termination.toISOString()
+        }
+    };
+});
+
 server.listen(8080, "::");
