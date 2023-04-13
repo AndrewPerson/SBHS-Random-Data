0b242e47ca2620abd13755cbe5b5835d39ad639b
Andrew Pye <professor_smart@hotmail.com>
2023-04-13 10:08:41 +1000

    Removed paragon specific endpoints

--- server.js
+++ server.js
@@ -48,38 +48,6 @@ else server.get("/api/timetable/timetable.json", wrapper(timetable));
 if (argv.userinfo) server.get("/api/details/userinfo.json", read(argv.userinfo))
 else server.get("/api/details/userinfo.json", wrapper(userinfo));
 
-server.get("/resources", async (req, res) => {
-    var termination = new Date();
-    //90 Days
-    termination.setHours(termination.getHours() + 90 * 24);
-
-    return {
-        result: {
-            announcements: await fetch("http://localhost:8080/api/dailynews/list.json").then(res => res.json()),
-            dailytimetable: await fetch("http://localhost:8080/api/timetable/daytimetable.json").then(res => res.json()),
-            "next-dailytimetable": await fetch("http://localhost:8080/api/timetable/daytimetable.json").then(res => res.json()),
-            timetable: await fetch("http://localhost:8080/api/timetable/timetable.json").then(res => res.json()),
-            userinfo: await fetch("http://localhost:8080/api/details/userinfo.json").then(res => res.json())
-        },
-        token: {
-            ...await fetch("http://localhost:8080/auth", { method: "POST" }).then(res => res.json()),
-            termination: termination.toISOString()
-        }
-    };
-});
-
-server.post("/auth", async (req, res) => {
-    var expiry = new Date();
-    expiry.setHours(expiry.getHours() + 1);
-
-    return {
-        access_token: "",
-        refresh_token: "",
-        expiry: expiry.toISOString(),
-        scope: "all-ro"
-    };
-});
-
 server.listen({
     port: argv.port ?? 8080
 });
